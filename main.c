4de7c811cba7d700e98c9f46086ea3803e1d29f4
Jake Day <jakeday@Jakes-MacBook-Pro-3.local>
2018-01-29 15:18:15 -0800

    First commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: jakeday
+ *
+ * Created on January 29, 2018, 3:16 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
