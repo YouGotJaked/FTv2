5d36addc0d1389f1e31c2a9209d0e29eee7f230a
Jake Day <jakeday@Jakes-MacBook-Pro-3.local>
2018-02-01 10:50:15 -0800

    Added sample code to verify connection works

--- client.c
+++ client.c
@@ -7,12 +7,71 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include "protocol.h"
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+#define SIZE 1024
 
-    return (EXIT_SUCCESS);
+int main(int argc, char **argv) {
+    int sock, port, bytes;
+    char buffer[SIZE];
+    struct sockaddr_in serv_addr;
+    socklen_t addr_size;
+    
+    if (argc != 3) {
+        printf("Usage: %s <port number> <ip of server>\n", argv[0]);
+        return -1;
+    }
+    
+    //configure address
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(atoi(argv[1]));
+    
+    if (inet_pton(AF_INET, argv[2], &serv_addr.sin_addr) <= 0) {
+        perror("inet_pton error occured");
+        return -1;
+    }
+    
+    memset(serv_addr.sin_zero, '\0', sizeof(serv_addr.sin_zero));
+    addr_size = sizeof(serv_addr);
+    
+    //create UDP socket for server
+    if ((sock = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Could not create socket");
+        return -1;
+    }
+    
+    while (1) {
+        fgets(buffer, SIZE, stdin);
+        bytes = strlen(buffer) + 1;
+        
+        printf("Sending: %s\n", buffer);
+        sendto(sock, buffer, bytes, 0, (struct sockaddr *)&serv_addr, addr_size);
+        
+        bytes = recvfrom(sock, buffer, SIZE, 0, NULL, NULL);
+        printf("Received: %s\n", buffer);
+        
+        close(sock);
+        return 0;
+    }
 }
-
+/*
+int chksum(char *buffer, size_t buff_size) {
+    char *p,*cs;
+    p = (char *)buffer;
+    cs = p++;
+    
+    for (int i = 0; i < buff_size; i++) {
+        cs = cs ^ *p;
+        cs++;
+        p++;
+    }
+    
+    return (int)cs;
+}
+*/
--- server.c
+++ server.c
@@ -1,5 +1,5 @@
 /* 
- * File:   main.c
+ * File:   server.c
  * Author: jakeday
  *
  * Created on January 29, 2018, 3:16 PM
@@ -7,12 +7,62 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include "protocol.h"
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+#include <ctype.h>
 
-    return (EXIT_SUCCESS);
+#define SIZE 1024
+
+int main(int argc, char **argv) {
+    int sock, bytes;
+    char buffer[SIZE];
+    struct sockaddr_in serv_addr, client_addr;
+    struct sockaddr_storage serv_storage;
+    socklen_t addr_size, client_addr_size;
+    
+    if (argc != 2) {
+        printf("Usage: %s <port number>\n", argv[0]);
+        return -1;
+    }
+    
+    //initialize
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(atoi(argv[1]));
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    
+    //zero server
+    memset((char *)serv_addr.sin_zero, '\0', sizeof(serv_addr.sin_zero));
+    addr_size = sizeof(serv_storage);
+    
+    //create UDP socket
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Socket error");
+        return -1;
+    }
+    
+    //bind socket
+    if (bind(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0) {
+        perror("Bind error");
+        return -1;
+    }
+    
+    //loop forever
+    while (1) {
+        bytes = recvfrom(sock, buffer, SIZE, 0, (struct sockaddr *)&serv_storage, &addr_size);
+        
+        for (int i = 0; i < bytes - 1; i++) {
+            buffer[i] = tolower(buffer[i]);
+        }
+        
+        sendto(sock, buffer, bytes, 0, (struct sockaddr *)&serv_storage, addr_size);
+        
+        close(sock);
+        return 0;
+    }
 }
 
