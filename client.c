2f88e7b3c121d5e96f16bddeff0b6e1beedd8c5b
Jake Day <jakeday@Jakes-MacBook-Pro-3.local>
2018-02-01 09:23:39 -0800

    Changed file names

---
+++ client.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   client.c
+ * Author: jakeday
+ *
+ * Created on February 1, 2018, 9:18 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
